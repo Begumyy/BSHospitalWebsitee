3bb91af68c573d5dcf9d4a266ad5268036fdf618
Savas Serbest <savas.serbest92@gmail.com>
2023-11-13 01:21:48 +0300

    Eklendi

--- BSHospitalWebsitee/Program.cs
+++ BSHospitalWebsitee/Program.cs
@@ -12,6 +12,9 @@ namespace BSHospitalWebsitee
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
+
+
             // Add services to the container.
             builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
             {
