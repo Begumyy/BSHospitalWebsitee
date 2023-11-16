5200290a684d948bf2bd14405a11f21b445bb699
Savas Serbest <savas.serbest92@gmail.com>
2023-11-16 14:51:17 +0300

    Departman ve Doktor adları listelendi index sayfasına getirildi

--- BSHospitalWebsitee/Controllers/DepartmentController.cs
+++ BSHospitalWebsitee/Controllers/DepartmentController.cs
@@ -1,46 +1,45 @@
-﻿using BSHospital.Models;
+﻿using BSHospital.Data;
+using BSHospital.Models;
 using BSHospital.Repository.Shared.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BSHospitalWebsitee.Controllers
 {
-    public class DepartmentController : ControllerBase
+    public class DepartmentController : Controller
     {
-        public DepartmentController(IUnitOfWork unitOfWork):base(unitOfWork)
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DepartmentController(IUnitOfWork unitOfWork)
         {
-            
-        }
-        public IActionResult Index()
-        {
-            return View();
+            _unitOfWork = unitOfWork;
         }
 
         [HttpPost]
         public IActionResult Add(Department department)
         {
-            unitOfWork.Departments.Add(department);
-            unitOfWork.Save();
+            _unitOfWork.Departments.Add(department);
+            _unitOfWork.Save();
             return View();
         }
 
         public IActionResult GetAll()
         {
-            return Json(new {data=unitOfWork.Departments.GetAll().ToList()});
+            return Json(_unitOfWork.Departments.GetAll().ToList());
         }
 
         [HttpPost]
         public IActionResult DeleteById(int id)
         {
-            unitOfWork.Departments.DeleteById(id);
-            unitOfWork.Save();
+            _unitOfWork.Departments.DeleteById(id);
+            _unitOfWork.Save();
             return Ok("Başarıyla silindi");
         }
 
         [HttpPost]
         public IActionResult Update(Department department)
         {
-            unitOfWork.Departments.Update(department);
-            unitOfWork.Save();
+            _unitOfWork.Departments.Update(department);
+            _unitOfWork.Save();
             return Ok();
         }
 
